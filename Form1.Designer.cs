57bb1600fb030db8112e5ff351f619dafa984142
Akanussus <87330595+Akanussus@users.noreply.github.com>
2023-03-21 22:21:11 +0100

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,26 +29,25 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.progressBar2 = new System.Windows.Forms.ProgressBar();
-            this.progressBar3 = new System.Windows.Forms.ProgressBar();
-            this.dlpercentage1 = new System.Windows.Forms.Label();
-            this.dlpercentage3 = new System.Windows.Forms.Label();
             this.dlpercentage2 = new System.Windows.Forms.Label();
+            this.dlpercentage1 = new System.Windows.Forms.Label();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.progressBar4 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dlpercentage3 = new System.Windows.Forms.Label();
+            this.dlpercentage4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // progressBar1
-            // 
-            this.progressBar1.Location = new System.Drawing.Point(12, 12);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(250, 46);
-            this.progressBar1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 168);
+            this.button1.Location = new System.Drawing.Point(12, 146);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(120, 23);
             this.button1.TabIndex = 1;
@@ -59,7 +58,7 @@
             // button2
             // 
             this.button2.Enabled = false;
-            this.button2.Location = new System.Drawing.Point(142, 168);
+            this.button2.Location = new System.Drawing.Point(142, 146);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(120, 23);
             this.button2.TabIndex = 2;
@@ -67,67 +66,136 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 42);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(250, 46);
+            this.progressBar1.TabIndex = 3;
+            // 
             // progressBar2
             // 
-            this.progressBar2.Location = new System.Drawing.Point(12, 64);
+            this.progressBar2.Location = new System.Drawing.Point(12, 94);
             this.progressBar2.Name = "progressBar2";
             this.progressBar2.Size = new System.Drawing.Size(250, 46);
-            this.progressBar2.TabIndex = 3;
-            // 
-            // progressBar3
-            // 
-            this.progressBar3.Location = new System.Drawing.Point(12, 116);
-            this.progressBar3.Name = "progressBar3";
-            this.progressBar3.Size = new System.Drawing.Size(250, 46);
-            this.progressBar3.TabIndex = 4;
-            // 
-            // dlpercentage1
-            // 
-            this.dlpercentage1.AutoSize = true;
-            this.dlpercentage1.BackColor = System.Drawing.Color.White;
-            this.dlpercentage1.Location = new System.Drawing.Point(268, 27);
-            this.dlpercentage1.Name = "dlpercentage1";
-            this.dlpercentage1.Size = new System.Drawing.Size(23, 15);
-            this.dlpercentage1.TabIndex = 5;
-            this.dlpercentage1.Text = "0%";
-            // 
-            // dlpercentage3
-            // 
-            this.dlpercentage3.AutoSize = true;
-            this.dlpercentage3.BackColor = System.Drawing.Color.White;
-            this.dlpercentage3.Location = new System.Drawing.Point(268, 132);
-            this.dlpercentage3.Name = "dlpercentage3";
-            this.dlpercentage3.Size = new System.Drawing.Size(23, 15);
-            this.dlpercentage3.TabIndex = 6;
-            this.dlpercentage3.Text = "0%";
+            this.progressBar2.TabIndex = 4;
             // 
             // dlpercentage2
             // 
             this.dlpercentage2.AutoSize = true;
             this.dlpercentage2.BackColor = System.Drawing.Color.White;
-            this.dlpercentage2.Location = new System.Drawing.Point(268, 80);
+            this.dlpercentage2.Location = new System.Drawing.Point(268, 111);
             this.dlpercentage2.Name = "dlpercentage2";
             this.dlpercentage2.Size = new System.Drawing.Size(23, 15);
-            this.dlpercentage2.TabIndex = 7;
+            this.dlpercentage2.TabIndex = 6;
             this.dlpercentage2.Text = "0%";
+            // 
+            // dlpercentage1
+            // 
+            this.dlpercentage1.AutoSize = true;
+            this.dlpercentage1.BackColor = System.Drawing.Color.White;
+            this.dlpercentage1.Location = new System.Drawing.Point(268, 60);
+            this.dlpercentage1.Name = "dlpercentage1";
+            this.dlpercentage1.Size = new System.Drawing.Size(23, 15);
+            this.dlpercentage1.TabIndex = 7;
+            this.dlpercentage1.Text = "0%";
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(344, 42);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(250, 46);
+            this.progressBar3.TabIndex = 8;
+            // 
+            // progressBar4
+            // 
+            this.progressBar4.Location = new System.Drawing.Point(344, 94);
+            this.progressBar4.Name = "progressBar4";
+            this.progressBar4.Size = new System.Drawing.Size(250, 46);
+            this.progressBar4.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(95, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 15);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Jevil Simulator";
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(474, 146);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(120, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Launch";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(344, 146);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Download";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(417, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 15);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Spamton Simulator";
+            // 
+            // dlpercentage3
+            // 
+            this.dlpercentage3.AutoSize = true;
+            this.dlpercentage3.BackColor = System.Drawing.Color.White;
+            this.dlpercentage3.Location = new System.Drawing.Point(600, 60);
+            this.dlpercentage3.Name = "dlpercentage3";
+            this.dlpercentage3.Size = new System.Drawing.Size(23, 15);
+            this.dlpercentage3.TabIndex = 14;
+            this.dlpercentage3.Text = "0%";
+            // 
+            // dlpercentage4
+            // 
+            this.dlpercentage4.AutoSize = true;
+            this.dlpercentage4.BackColor = System.Drawing.Color.White;
+            this.dlpercentage4.Location = new System.Drawing.Point(600, 111);
+            this.dlpercentage4.Name = "dlpercentage4";
+            this.dlpercentage4.Size = new System.Drawing.Size(23, 15);
+            this.dlpercentage4.TabIndex = 15;
+            this.dlpercentage4.Text = "0%";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(304, 201);
-            this.Controls.Add(this.dlpercentage2);
+            this.ClientSize = new System.Drawing.Size(644, 177);
+            this.Controls.Add(this.dlpercentage4);
             this.Controls.Add(this.dlpercentage3);
-            this.Controls.Add(this.dlpercentage1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar4);
             this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.dlpercentage1);
+            this.Controls.Add(this.dlpercentage2);
             this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.progressBar1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(320, 240);
-            this.MinimumSize = new System.Drawing.Size(320, 240);
+            this.MaximumSize = new System.Drawing.Size(660, 216);
+            this.MinimumSize = new System.Drawing.Size(660, 216);
             this.Name = "Form1";
             this.Text = "Jevil Simulator Launcher";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -137,14 +205,19 @@
         }
 
         #endregion
-
-        private ProgressBar progressBar1;
         private Button button1;
         private Button button2;
+        private ProgressBar progressBar1;
         private ProgressBar progressBar2;
-        private ProgressBar progressBar3;
-        private Label dlpercentage1;
-        private Label dlpercentage3;
         private Label dlpercentage2;
+        private Label dlpercentage1;
+        private ProgressBar progressBar3;
+        private ProgressBar progressBar4;
+        private Label label1;
+        private Button button4;
+        private Button button3;
+        private Label label2;
+        private Label dlpercentage3;
+        private Label dlpercentage4;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,6 +1,7 @@
 using System.Net;
 using System.IO.Compression;
 using System.Diagnostics;
+using System.ComponentModel;
 
 namespace JevilSim
 {
@@ -17,6 +18,10 @@ namespace JevilSim
             {
                 button2.Enabled = true;
             }
+            if (Directory.Exists(".\\SpamtonSim\\"))
+            {
+                button4.Enabled = true;
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -25,43 +30,63 @@ namespace JevilSim
             {
                 wc.DownloadProgressChanged += wc_DownloadProgressChanged1;
                 wc.DownloadFileAsync(new Uri("https://www.dropbox.com/s/9fo502w8usgp7ll/JevilSim.zip?dl=1"), "JevilSim.zip");
-                wc.DownloadFileCompleted += wc_DownloadFileCompleted;
+                wc.DownloadFileCompleted += wc_DownloadFileCompleted1;
                 
             }
             using (WebClient wc = new WebClient())
             {
                 wc.DownloadProgressChanged += wc_DownloadProgressChanged2;
-                wc.DownloadFileAsync(new Uri("https://www.dropbox.com/s/l5fwdua88ynwnhw/filech1_jevilsim?dl=1"), "filech1_jevilsim");
+                wc.DownloadFileAsync(new Uri("https://www.dropbox.com/s/wxup87njhpvq0re/DELTARUNE.zip?dl=1"), "DELTARUNE.zip");
+                wc.DownloadFileCompleted += wc_DownloadFileCompleted2;
             }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(Directory.GetCurrentDirectory() + @"\JevilSim\DELTARUNE.exe"));
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
             using (WebClient wc = new WebClient())
             {
                 wc.DownloadProgressChanged += wc_DownloadProgressChanged3;
-                wc.DownloadFileAsync(new Uri("https://www.dropbox.com/s/8c78sgl9ufgp9sn/drjs.ini?dl=1"), "drjs.ini");
+                wc.DownloadFileAsync(new Uri("https://www.dropbox.com/s/0ypz388ji96opk8/SpamtonSim.zip?dl=1"), "SpamtonSim.zip");
+                wc.DownloadFileCompleted += wc_DownloadFileCompleted3;
+            }
+            using (WebClient wc = new WebClient())
+            {
+                wc.DownloadProgressChanged += wc_DownloadProgressChanged4;
+                wc.DownloadFileAsync(new Uri("https://www.dropbox.com/s/os8p9o2tblykhd4/SpamtonSexSimulator.zip?dl=1"), "SpamtonSexSimulator.zip");
+                wc.DownloadFileCompleted += wc_DownloadFileCompleted4;
             }
         }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(Directory.GetCurrentDirectory() + @"\SpamtonSim\SpamtonSexSimulator.exe"));
+        }
 
-        private void wc_DownloadFileCompleted(object? sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        private void wc_DownloadFileCompleted1(object? sender, AsyncCompletedEventArgs e)
         {
             ZipFile.ExtractToDirectory("JevilSim.zip", Directory.GetCurrentDirectory(), true);
-            string localAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-            if (Directory.Exists(localAppData + "\\DELTARUNE\\"))
-            {
-                File.Move("filech1_jevilsim", localAppData + "\\DELTARUNE\\filech1_jevilsim", true);
-                File.Move("drjs.ini", localAppData + "\\DELTARUNE\\drjs.ini", true);
-            }
-            else
-            {
-                Directory.CreateDirectory(localAppData + "\\DELTARUNE\\");
-                File.Move("filech1_jevilsim", localAppData + "\\DELTARUNE\\filech1_jevilsim", true);
-                File.Move("drjs.ini", localAppData + "\\DELTARUNE\\drjs.ini", true);
-            }
             button2.Enabled = true;
+        }
+        private void wc_DownloadFileCompleted2(object? sender, AsyncCompletedEventArgs e)
+        {
+            ZipFile.ExtractToDirectory("DELTARUNE.zip", Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), true);
+        }
+        private void wc_DownloadFileCompleted3(object? sender, AsyncCompletedEventArgs e)
+        {
+            ZipFile.ExtractToDirectory("SpamtonSim.zip", Directory.GetCurrentDirectory(), true);
+            button4.Enabled = true;
+        }
+        private void wc_DownloadFileCompleted4(object? sender, AsyncCompletedEventArgs e)
+        {
+            ZipFile.ExtractToDirectory("SpamtonSexSimulator.zip", Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), true);
         }
 
         void wc_DownloadProgressChanged1(object sender, DownloadProgressChangedEventArgs e)
         {
             progressBar1.Value = e.ProgressPercentage;
-            dlpercentage1.Text = e.ProgressPercentage.ToString() + "%";
+            dlpercentage1.Text = e.ProgressPercentage.ToString() + "%"; 
         }
         void wc_DownloadProgressChanged2(object sender, DownloadProgressChangedEventArgs e)
         {
@@ -73,10 +98,10 @@ namespace JevilSim
             progressBar3.Value = e.ProgressPercentage;
             dlpercentage3.Text = e.ProgressPercentage.ToString() + "%";
         }
-
-        private void button2_Click(object sender, EventArgs e)
+        void wc_DownloadProgressChanged4(object sender, DownloadProgressChangedEventArgs e)
         {
-            Process.Start(new ProcessStartInfo(Directory.GetCurrentDirectory() + @"\JevilSim\DELTARUNE.exe"));
+            progressBar4.Value = e.ProgressPercentage;
+            dlpercentage4.Text = e.ProgressPercentage.ToString() + "%";
         }
     }
 }
